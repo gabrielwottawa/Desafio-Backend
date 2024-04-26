2a18d84f509451bc4823d06c7c6c00dc2d9932a9
Gabriel Wottawa <gabrielclerio10@gmail.com>
2024-04-26 11:03:59 -0300

    criação de endpoint para remoção de moto e criação de transactions para as querys postgreSQL

--- MotorbikeRental/MotorbikeRental.Infrastructure/Repositories/MotorbikeRepository.cs
+++ MotorbikeRental/MotorbikeRental.Infrastructure/Repositories/MotorbikeRepository.cs
@@ -2,12 +2,14 @@
 using MotorbikeRental.Infrastructure.Models;
 using MotorbikeRental.Infrastructure.PostgreSQL;
 using MotorbikeRental.Infrastructure.Repositories.IRepositories;
+using Npgsql;
 
 namespace MotorbikeRental.Infrastructure.Repositories
 {
     public class MotorbikeRepository : IMotorbikeRepository
     {
         private readonly PostgreSQLDatabaseContext _postgreSQLDatabaseContext;
+        private NpgsqlTransaction _transaction;
 
         public MotorbikeRepository(PostgreSQLDatabaseContext postgreSQLDatabaseContext)
         {
@@ -16,49 +18,148 @@ namespace MotorbikeRental.Infrastructure.Repositories
 
         public async Task<IEnumerable<Motorbikes>> GetAllMotorbikes(string? plate)
         {
-            var sqlCommand = "SELECT * FROM motorbike";
+            BeginTransaction();
 
-            if (plate != null)
-                sqlCommand += " WHERE plate = @plate";
+            try
+            {
+                var sqlCommand = "SELECT * FROM motorbike";
 
-            return await _postgreSQLDatabaseContext.Connection.QueryAsync<Motorbikes>(sqlCommand, new { plate });
+                if (plate != null)
+                    sqlCommand += " WHERE plate = @plate";
+
+                var result = await _postgreSQLDatabaseContext.Connection.QueryAsync<Motorbikes>(
+                                sqlCommand
+                                , new { plate }
+                                , _transaction);
+
+                _transaction.Commit();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                _transaction.Rollback();
+                throw;
+            }
         }
 
         public async Task<Motorbikes> GetMotorbikeByPlate(string plate)
         {
-            return await _postgreSQLDatabaseContext.Connection.QuerySingleOrDefaultAsync<Motorbikes>(
+            BeginTransaction();
+
+            try
+            {
+                var result = await _postgreSQLDatabaseContext.Connection.QuerySingleOrDefaultAsync<Motorbikes>(
                         @"SELECT 
                             * 
                         FROM 
                             motorbike
                         WHERE
-                            plate = @plate", new { plate });
+                            plate = @plate"
+                        , new { plate }
+                        , _transaction
+                        );
+
+                _transaction.Commit();
+
+                return result;
+            }
+            catch (Exception) 
+            {
+                _transaction.Rollback();
+                throw;
+            }
         }
 
         public async Task<Motorbikes> GetMotorbikeById(int id)
         {
-            return await _postgreSQLDatabaseContext.Connection.QuerySingleOrDefaultAsync<Motorbikes>(
+            BeginTransaction();
+
+            try
+            {
+                var result =  await _postgreSQLDatabaseContext.Connection.QuerySingleOrDefaultAsync<Motorbikes>(
                         @"SELECT 
                             * 
                         FROM 
                             motorbike
                         WHERE
-                            id = @id", new { id });
+                            id = @id"
+                        , new { id }
+                        , _transaction
+                        );
+
+                _transaction.Commit();
+
+                return result;
+            } 
+            catch (Exception)
+            {
+                _transaction.Rollback();
+                throw;
+            }
         }
 
         public async Task InsertMotorbike(Motorbikes motorbike)
         {
-            await _postgreSQLDatabaseContext.Connection.ExecuteAsync(
+            BeginTransaction();
+
+            try
+            {
+                await _postgreSQLDatabaseContext.Connection.ExecuteAsync(
                         @"INSERT INTO motorbike(plate, year, type)
 	                            VALUES (@plate, @year, @type)"
-                        , new { motorbike.Plate, motorbike.Year, motorbike.Type });
+                        , new { motorbike.Plate, motorbike.Year, motorbike.Type }
+                        , _transaction);
+
+                _transaction.Commit();
+            }
+            catch (Exception)
+            {
+                _transaction.Rollback();
+            }
         }
 
         public async Task UpdateMotorbike(int id, string plate)
         {
-            await _postgreSQLDatabaseContext.Connection.ExecuteAsync(
+            BeginTransaction();
+
+            try
+            {
+                await _postgreSQLDatabaseContext.Connection.ExecuteAsync(
                     @"UPDATE motorbike SET plate = @plate WHERE id = @id"
-                    , new { id, plate });
+                    , new { id, plate }
+                    , _transaction);
+
+                _transaction.Commit();
+            }
+            catch (Exception)
+            {
+                _transaction.Rollback();
+            }
+        }
+
+        public async Task DeleteMotorbikeById(int id)
+        {
+            BeginTransaction();
+
+            try
+            {
+                await _postgreSQLDatabaseContext.Connection.ExecuteAsync(
+                            @"DELETE FROM motorbike WHERE id = @id"
+                            , new { id }
+                            , _transaction);
+
+                _transaction.Commit();
+            }
+            catch (Exception)
+            {
+                _transaction.Rollback();
+            }
+        }
+
+        private void BeginTransaction()
+        {
+            _transaction = _postgreSQLDatabaseContext.Connection.BeginTransaction();
         }
     }
 }
